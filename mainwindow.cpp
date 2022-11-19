a57472104962d580e524418c8a3712fa0b825d5d
CSReviser <CSReviser@gmai.com>
2022-11-20 00:16:05 +0900

    任意のらじる★らじるの「聴き逃し」配信講座に対応（試行対応）

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -71,14 +71,14 @@
 #define SETTING_OPT_TITLE2 "opt_title2"
 #define SETTING_OPT_TITLE3 "opt_title3"
 #define SETTING_OPT_TITLE4 "opt_title4"
-#define OPTIONAL1 "0953"
-#define OPTIONAL2 "4412"
-#define OPTIONAL3 "0937"
-#define OPTIONAL4 "2769"
-#define Program_TITLE1 "任意らじる番組１"
-#define Program_TITLE2 "任意らじる番組２"
-#define Program_TITLE3 "任意らじる番組３"
-#define Program_TITLE4 "任意らじる番組４"
+#define OPTIONAL1 "7155"	// Living in Japan
+#define OPTIONAL2 "0701"	// やさしい日本語
+#define OPTIONAL3 "7629"	// Learn Japanese from the News
+#define OPTIONAL4 "7512"	// ニュースで学ぶ「現代英語」
+#define Program_TITLE1 "Living in Japan"
+#define Program_TITLE2 "やさしい日本語"
+#define Program_TITLE3 "Learn Japanese from the News"
+#define Program_TITLE4 "ニュースで学ぶ「現代英語」"
 
 #ifdef QT4_QT5_WIN
 #define STYLE_SHEET "stylesheet-win.qss"
@@ -105,7 +105,7 @@ namespace {
 //			int day = regexp.cap( 2 ).toInt();
 //			result = QString( " (%1/%2/%3)" ).arg( regexp.cap( 3 ) )
 //					.arg( month, 2, 10, QLatin1Char( '0' ) ).arg( day, 2, 10, QLatin1Char( '0' ) );
-			result = QString( " (2022/11/16)" ); 
+			result = QString( " (2022/11/20)" ); 
 		}
 		return result;
 	}
@@ -173,6 +173,10 @@ MainWindow::MainWindow( QWidget *parent )
 	connect( action, SIGNAL( triggered() ), this, SLOT( customizeTitle() ) );
 	customizeMenu->addAction( action );
 
+	action = new QAction( QString::fromUtf8( "任意番組設定..." ), this );
+	connect( action, SIGNAL( triggered() ), this, SLOT( customizeScramble() ) );
+	customizeMenu->addAction( action );
+	
 	//action = new QAction( QString::fromUtf8( "スクランブル文字列..." ), this );
 	//connect( action, SIGNAL( triggered() ), this, SLOT( customizeScramble() ) );
 	//customizeMenu->addAction( action );
@@ -321,6 +325,44 @@ void MainWindow::settings( enum ReadWriteMode mode ) {
 		scrambleUrl1 = saved.type() == QVariant::Invalid ? SCRAMBLE_URL1 : saved.toString();
 		saved = settings.value( SETTING_SCRAMBLE_URL2 );
 		scrambleUrl2 = saved.type() == QVariant::Invalid ? SCRAMBLE_URL2 : saved.toString();
+		
+		saved = settings.value( SETTING_OPTIONAL1 );
+		optional1 = saved.type() == QVariant::Invalid ? OPTIONAL1 : saved.toString();
+		saved = settings.value( SETTING_OPTIONAL2 );
+		optional2 = saved.type() == QVariant::Invalid ? OPTIONAL2 : saved.toString();
+		saved = settings.value( SETTING_OPTIONAL3 );
+		optional3 = saved.type() == QVariant::Invalid ? OPTIONAL3 : saved.toString();
+		saved = settings.value( SETTING_OPTIONAL4 );
+		optional4 = saved.type() == QVariant::Invalid ? OPTIONAL4 : saved.toString();
+
+		saved = settings.value( SETTING_OPT_TITLE1 );
+		program_title1 = saved.type() == QVariant::Invalid ? QString::fromUtf8( Program_TITLE1 ) : saved.toString();
+		saved = settings.value( SETTING_OPT_TITLE2 );
+		program_title2 = saved.type() == QVariant::Invalid ? QString::fromUtf8( Program_TITLE2 ) : saved.toString();
+		saved = settings.value( SETTING_OPT_TITLE3 );
+		program_title3 = saved.type() == QVariant::Invalid ? QString::fromUtf8( Program_TITLE3 ) : saved.toString();
+		saved = settings.value( SETTING_OPT_TITLE4 );
+		program_title4 = saved.type() == QVariant::Invalid ? QString::fromUtf8( Program_TITLE4 ) : saved.toString();
+
+		ui->toolButton_optional1->setText( QString( program_title1 ) );
+		ui->toolButton_optional2->setText( QString( program_title2 ) );
+		ui->toolButton_optional3->setText( QString( program_title3 ) );
+		ui->toolButton_optional4->setText( QString( program_title4 ) );
+
+//		QString opt_TITLE1 = getJsonData( optional1 );
+//		QString opt_TITLE2 = getJsonData( optional2 );
+//		QString opt_TITLE3 = getJsonData( optional3 );
+//		QString opt_TITLE4 = getJsonData( optional4 );
+		
+//		program_title1 = opt_TITLE1;
+//		program_title2 = opt_TITLE2;
+//		program_title3 = opt_TITLE3;
+//		program_title4 = opt_TITLE4;
+//		
+		ui->toolButton_optional1->setText( QString( program_title1 ) );
+		ui->toolButton_optional2->setText( QString( program_title2 ) );
+		ui->toolButton_optional3->setText( QString( program_title3 ) );
+		ui->toolButton_optional4->setText( QString( program_title4 ) );
 
 		for ( int i = 0; checkBoxes[i].checkBox != NULL; i++ ) {
 			checkBoxes[i].checkBox->setChecked( settings.value( checkBoxes[i].key, checkBoxes[i].defaultValue ).toBool() );
@@ -340,6 +382,16 @@ void MainWindow::settings( enum ReadWriteMode mode ) {
 		settings.setValue( SETTING_SCRAMBLE, scramble );
 		settings.setValue( SETTING_SCRAMBLE_URL1, scrambleUrl1 );
 		settings.setValue( SETTING_SCRAMBLE_URL2, scrambleUrl2 );
+		
+		settings.setValue( SETTING_OPTIONAL1, optional1 );
+		settings.setValue( SETTING_OPTIONAL2, optional2 );
+		settings.setValue( SETTING_OPTIONAL3, optional3 );
+		settings.setValue( SETTING_OPTIONAL4, optional4 );
+		settings.setValue( SETTING_OPT_TITLE1, program_title1 );
+		settings.setValue( SETTING_OPT_TITLE2, program_title2 );
+		settings.setValue( SETTING_OPT_TITLE3, program_title3 );
+		settings.setValue( SETTING_OPT_TITLE4, program_title4 );
+		
 		for ( int i = 0; checkBoxes[i].checkBox != NULL; i++ ) {
 			settings.setValue( checkBoxes[i].key, checkBoxes[i].checkBox->isChecked() );
 		}
@@ -372,9 +424,30 @@ void MainWindow::customizeSaveFolder() {
 }
 
 void MainWindow::customizeScramble() {
-	ScrambleDialog dialog( scramble );
+//	ScrambleDialog dialog( scramble );
+//	dialog.exec();
+//	scramble = dialog.scramble();
+
+	ScrambleDialog dialog( optional1, optional2, optional3, optional4 );
 	dialog.exec();
-	scramble = dialog.scramble();
+	optional1 = dialog.scramble1();
+	optional2 = dialog.scramble2();
+	optional3 = dialog.scramble3();
+	optional4 = dialog.scramble4();
+
+	QString opt_TITLE1 = getJsonData( optional1.left(4) );
+	QString opt_TITLE2 = getJsonData( optional2.left(4) );
+	QString opt_TITLE3 = getJsonData( optional3.left(4) );
+	QString opt_TITLE4 = getJsonData( optional4.left(4) );
+
+	program_title1 = opt_TITLE1;
+	ui->toolButton_optional1->setText( QString( program_title1 ) );
+	program_title2 = opt_TITLE2;
+	ui->toolButton_optional2->setText( QString( program_title2 ) );
+	program_title3 = opt_TITLE3;
+	ui->toolButton_optional3->setText( QString( program_title3 ) );
+	program_title4 = opt_TITLE4;
+	ui->toolButton_optional4->setText( QString( program_title4 ) );
 }
 
 void MainWindow::download() {	//「レコーディング」または「キャンセル」ボタンが押されると呼び出される
@@ -395,6 +468,38 @@ void MainWindow::download() {	//「レコーディング」または「キャン
 		downloadThread->disconnect();	//wait中にSIGNALが発生するとデッドロックするためすべてdisconnect
 		finished();
 	}
+}
+
+QString MainWindow::getJsonData( QString url ) {
+	QString attribute;
+	attribute.clear() ;
+    	QEventLoop eventLoop;
+	QNetworkAccessManager mgr;
+ 	QObject::connect(&mgr, SIGNAL(finished(QNetworkReply*)), &eventLoop, SLOT(quit()));
+	const QString jsonUrl = json_prefix + url + "/bangumi_" + url + "_01.json";
+	QUrl url_json( jsonUrl );
+	QNetworkRequest req;
+	req.setUrl(url_json);
+	QNetworkReply *reply = mgr.get(req);
+	eventLoop.exec(); 
+	
+	if (reply->error() == QNetworkReply::NoError) {
+		QString strReply = (QString)reply->readAll();
+		QJsonDocument jsonResponse = QJsonDocument::fromJson(strReply.toUtf8());
+		QJsonObject jsonObject = jsonResponse.object();
+		QJsonObject jsonObj = jsonResponse.object();
+    
+		QJsonArray jsonArray = jsonObject[ "main" ].toArray();
+		QJsonObject objx2 = jsonObject[ "main" ].toObject();
+		attribute = objx2[ "program_name" ].toString().replace( "　", " " );
+		    for (ushort i = 0xFF1A; i < 0xFF5F; ++i) {
+		        attribute = attribute.replace(QChar(i), QChar(i - 0xFEE0));
+		    }
+		    for (ushort i = 0xFF10; i < 0xFF1A; ++i) {
+		        attribute = attribute.replace( QChar(i - 0xFEE0), QChar(i) );
+		    }
+	}
+	return attribute;
 }
 
 void MainWindow::toggled( bool checked ) {
